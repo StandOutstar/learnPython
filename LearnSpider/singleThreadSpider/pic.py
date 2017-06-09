a440cf96f5f5fd3de30716cdc92fd52c139b32f9
StandOutstar <729901070@qq.com>
2017-06-09 18:56:24 +0800

    添加树、列表等。

--- LearnSpider/singleThreadSpider/pic.py
+++ LearnSpider/singleThreadSpider/pic.py
@@ -1,7 +1,12 @@
 import requests
 
+
+def response_hook(response, *args, **kwargs):
+    print(response.status_code)
+
 # r = requests.get('http://iflv9.afreecatv.com/AFFLV/03/7/45/9_03_1495952609691177_2147483647_L.jpg')
-r = requests.get('http://bpic.588ku.com/back_pic/04/28/17/53583d40b2444bc.jpg!/fh/300/quality/90/unsharp/true/compress/true')
+
+r = requests.get('http://bpic.588ku.com/back_pic/04/28/17/53583d40b2444bc.jpg!/fh/300/quality/90/unsharp/true/compress/true', hooks=dict(response=response_hook))
 
 with open('3.jpg', 'wb') as f:
     f.write(r.content)
